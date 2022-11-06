bf70eb2c497536c15ac9c7c7daefc13599021f0c
shadow578 <52449218+shadow578@users.noreply.github.com>
2022-11-06 15:58:07 +0100

    prepare app to run as a hassos addon

---
+++ sma-data-manager-bridge/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/with-contenv bashio
+set +u
+
+# load config and write to env 
+export SMA_HOST=$(bashio::config 'sma_host')
+export SMA_USERNAME=$(bashio::config 'sma_username')
+export SMA_PASSWORD=$(bashio::config 'sma_password')
+
+# start application
+bashio::log.info "starting connector"
+node .
+bashio::log.info "connector shut down"
